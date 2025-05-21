1f952222b338faf27c0e9fc20d3dd42d02e5f947
CokeFenya <danilov.danil90@mail.ru>
2025-05-21 16:14:00 +0300

    Добавить husky, commitlint и lint-staged конфигурации

--- dist/universal-payments.constants.d.ts
+++
@@ -1,2 +0,0 @@
-declare const UNITPAY_DEFAULT_URL = "https://unitpay.ru/api";
-export { UNITPAY_DEFAULT_URL as DEFAULT_URL };
--- dist/universal-payments.constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_URL = void 0;
-const UNITPAY_DEFAULT_URL = 'https://unitpay.ru/api';
-exports.DEFAULT_URL = UNITPAY_DEFAULT_URL;
