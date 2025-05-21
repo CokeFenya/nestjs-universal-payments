1a1a50915d50633febc9cb33a1330d95e79d3280
CokeFenya <danilov.danil90@mail.ru>
2025-05-21 15:43:25 +0300

    Добавить husky, commitlint и lint-staged конфигурации

--- dist/interfaces/unitpay.interface.d.ts
+++
@@ -1,8 +0,0 @@
-import type { FactoryProvider, ModuleMetadata } from '@nestjs/common';
-export declare const UnitPayOptionsSymbol: unique symbol;
-export type UnitPayOptions = {
-    projectId: string;
-    secretKey: string;
-    apiUrl: string;
-};
-export type UnitPayAsyncOptions = Pick<ModuleMetadata, 'imports'> & Pick<FactoryProvider<UnitPayOptions>, 'useFactory' | 'inject'>;
